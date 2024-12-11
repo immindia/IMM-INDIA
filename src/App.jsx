f1d120dc0d0bae848891081144a9766fe7ffe19c
whyehtasam <ehtasam2003197@st.jmi.ac.in>
2024-12-11 15:27:08 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button"
 function App() {
   return (
-    <Button className='px-6' variant='outline'>click me !</Button>
+    <Button className='px-6' variant=''>click me !</Button>
   );
 }
 
