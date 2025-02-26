2a73308a33a3a066a14c86f27367d4777ddbf8fc
whyehtasam <ehtasam2003197@st.jmi.ac.in>
2025-02-26 13:18:41 +0530

    Add Company Logo Assets and Refactor Campus Recruitment Component
    - Created companyLogoData.js to manage company logos for the Campus Recruitment section
    - Imported real company logo images to replace placeholder SVGs
    - Updated CampusRecruitment component to use imported logo data
    - Enhanced logo display with improved hover effects and blend modes
    - Added mix-blend-multiply class to improve logo visibility and aesthetics

---
+++ src/app/placements/companyLogoData.js
@@ -0,0 +1,345 @@
+/* eslint-disable no-unused-vars */
+import img1 from "../../assets/placements/companyLogo/1.png";
+import img2 from "../../assets/placements/companyLogo/2.png";
+import img3 from "../../assets/placements/companyLogo/3.png";
+import img4 from "../../assets/placements/companyLogo/4.png";
+import img5 from "../../assets/placements/companyLogo/5.png";
+import img6 from "../../assets/placements/companyLogo/6.png";
+import img7 from "../../assets/placements/companyLogo/7.png";
+import img8 from "../../assets/placements/companyLogo/8.png";
+import img9 from "../../assets/placements/companyLogo/9.png";
+import img10 from "../../assets/placements/companyLogo/10.png";
+import img11 from "../../assets/placements/companyLogo/11.png";
+import img12 from "../../assets/placements/companyLogo/12.png";
+import img13 from "../../assets/placements/companyLogo/13.png";
+import img14 from "../../assets/placements/companyLogo/14.png";
+import img15 from "../../assets/placements/companyLogo/15.png";
+import img16 from "../../assets/placements/companyLogo/16.png";
+import img17 from "../../assets/placements/companyLogo/17.png";
+import img18 from "../../assets/placements/companyLogo/18.png";
+import img19 from "../../assets/placements/companyLogo/19.png";
+import img20 from "../../assets/placements/companyLogo/20.png";
+import img21 from "../../assets/placements/companyLogo/21.png";
+import img22 from "../../assets/placements/companyLogo/22.png";
+import img23 from "../../assets/placements/companyLogo/23.png";
+import img24 from "../../assets/placements/companyLogo/24.png";
+import img25 from "../../assets/placements/companyLogo/25.png";
+import img26 from "../../assets/placements/companyLogo/26.png";
+import img27 from "../../assets/placements/companyLogo/27.png";
+import img28 from "../../assets/placements/companyLogo/28.png";
+import img29 from "../../assets/placements/companyLogo/29.png";
+import img30 from "../../assets/placements/companyLogo/30.png"; 
+import img31 from "../../assets/placements/companyLogo/31.png";
+import img32 from "../../assets/placements/companyLogo/32.png";
+import img33 from "../../assets/placements/companyLogo/33.png";
+import img34 from "../../assets/placements/companyLogo/34.png";
+import img35 from "../../assets/placements/companyLogo/35.png";
+import img36 from "../../assets/placements/companyLogo/36.png";
+import img37 from "../../assets/placements/companyLogo/37.png";
+import img38 from "../../assets/placements/companyLogo/38.png";
+import img39 from "../../assets/placements/companyLogo/39.png";
+import img40 from "../../assets/placements/companyLogo/40.png";
+import img41 from "../../assets/placements/companyLogo/41.png";
+import img42 from "../../assets/placements/companyLogo/42.png";
+import img43 from "../../assets/placements/companyLogo/43.png";
+import img44 from "../../assets/placements/companyLogo/44.png";
+import img45 from "../../assets/placements/companyLogo/45.png";
+import img46 from "../../assets/placements/companyLogo/46.png";
+import img47 from "../../assets/placements/companyLogo/47.png";
+import img48 from "../../assets/placements/companyLogo/48.png";
+import img49 from "../../assets/placements/companyLogo/49.png";
+import img50 from "../../assets/placements/companyLogo/50.png";
+// import img51 from "../../assets/placements/companyLogo/51.png";
+import img52 from "../../assets/placements/companyLogo/52.png";
+import img53 from "../../assets/placements/companyLogo/53.png";
+import img54 from "../../assets/placements/companyLogo/54.png";
+import img55 from "../../assets/placements/companyLogo/55.png";
+import img56 from "../../assets/placements/companyLogo/56.png";
+import img57 from "../../assets/placements/companyLogo/57.png";
+
+
+
+
+
+export const companyLogos = [
+    {
+      id: 1,
+      name: "PayU",
+      logo: img49,
+    },
+    {
+      id: 2,
+      name: "upGrad",
+      logo: img50,
+    },
+    {
+      id: 3,
+      name: "Grofers",
+      logo: img52,
+    },
+    {
+      id: 4,
+      name: "HDB Financial Services",
+      logo: img53,
+    },
+    {
+      id: 5,
+      name: "Genpact",
+      logo: img54,
+    },
+    {
+      id: 6,
+      name: "HomeFirst",
+      logo: img55,
+    },
+    {
+      id: 7,
+      name: "Entab",
+      logo: img56,
+    },
+    {
+      id: 8,
+      name: "Naukri.com",
+      logo: img57,
+    },
+    {
+      id: 9,
+      name: "JK Group",
+      logo: img41,
+    },
+    {
+      id: 10,
+      name: "S&P Global",
+      logo: img42,
+    },
+    {
+      id: 11,
+      name: "Puma",
+      logo: img43,
+    },
+    {
+      id: 12,
+      name: "Regalo Kitchens",
+      logo: img44,
+    },
+    {
+      id: 13,
+      name: "Jaro Education",
+      logo: img45,
+    },
+    {
+      id: 14,
+      name: "Shineroad",
+      logo: img46,
+    },
+    {
+      id: 15,
+      name: "Capgemini",
+      logo: img47,
+    },
+    {
+      id: 16,
+      name: "India Infrastructure",
+      logo: img48,
+    },
+    {
+      id: 17,
+      name: "DHL",
+      logo: img33,
+    },
+    {
+      id: 18,
+      name: "FinEdge",
+      logo: img34,
+    },
+    {
+      id: 19,
+      name: "PhonePe",
+      logo: img35,
+    },
+    {
+      id: 20,
+      name: "Ameriprise Financial",
+      logo: img36,
+    },
+    {
+      id: 21,
+      name: "VI",
+      logo: img37,
+    },
+    {
+      id: 22,
+      name: "IDFC First Bank",
+      logo: img38,
+    },
+    {
+      id: 23,
+      name: "Ather",
+      logo: img39,
+    },
+    {
+      id: 24,
+      name: "Intellipaat",
+      logo: img40,
+    },
+    {
+      id: 25,
+      name: "ICICI Bank",
+      logo: img25,
+    },
+    {
+      id: 26,
+      name: "TRB Group",
+      logo: img26,
+    },
+    {
+      id: 27,
+      name: "Blackstone",
+      logo: img27,
+    },
+    {
+      id: 28,
+      name: "JK Cement",
+      logo: img28,
+    },
+    {
+      id: 29,
+      name: "Chegg",
+      logo: img29,
+    },
+    {
+      id: 30,
+      name: "Solarman",
+      logo: img30,
+    },
+    {
+      id: 31,
+      name: "Vouchagram",
+      logo: img31,
+    },
+    {
+      id: 32,
+      name: "Anand Rathi",
+      logo: img32,
+    },
+    {
+      id: 33,
+      name: "RGF Professional Recruitment",
+      logo: img17,
+    },
+    {
+      id: 34,
+      name: "Archer & Bull",
+      logo: img18,
+    },
+    {
+      id: 35,
+      name: "Kotak Life",
+      logo: img19,
+    },
+    {
+      id: 36,
+      name: "Ceasefire",
+      logo: img20,
+    },
+    {
+      id: 37,
+      name: "Kirana Bazaar",
+      logo: img21,
+    },
+    {
+      id: 38,
+      name: "Sendinblue",
+      logo: img22,
+    },
+    {
+      id: 39,
+      name: "Hike Education",
+      logo: img23,
+    },
+    {
+      id: 40,
+      name: "EduKyu",
+      logo: img24,
+    },
+    {
+      id: 41,
+      name: "Rungta Irrigation Limited",
+      logo: img9,
+    },
+    {
+      id: 42,
+      name: "MarketsandMarkets",
+      logo: img10,
+    },
+    {
+      id: 43,
+      name: "PlanetSpark",
+      logo: img11,
+    },
+    {
+      id: 44,
+      name: "Digital Jalebi",
+      logo: img12,
+    },
+    {
+      id: 45,
+      name: "Acuité Ratings & Research",
+      logo: img13,
+    },
+    {
+      id: 46,
+      name: "AllCargo",
+      logo: img14,
+    },
+    {
+      id: 47,
+      name: "N-Squared eCommerce",
+      logo: img15,
+    },
+    {
+      id: 48,
+      name: "GirnarSoft",
+      logo: img16,
+    },
+    {
+      id: 49,
+      name: "To The New",
+      logo: img8,
+    },
+    {
+      id: 50,
+      name: "IDP",
+      logo: img7,
+    },
+    {
+      id: 51,
+      name: "Coffee Mug",
+      logo: img5,
+    },
+    {
+      id: 52,
+      name: "Antle Information Technologies",
+      logo: img6,
+    },
+    {
+      id: 53,
+      name: "Maison D’Auraine",
+      logo: img4,
+    },
+    {
+      id: 54,
+      name: "Pinnacle",
+      logo: img3,
+    },
+    {
+      id: 55,
+      name: "Corner Office Advisors",
+      logo: img4,
+    },
+    {
+      id: 56,
+      name: "Jumbo Group",
+      logo: img2,
+    },
+  ];
