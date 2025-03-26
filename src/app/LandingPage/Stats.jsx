82d729296b8845e14e411c8c1314499c82fb6767
whyehtasam <ehtasam2003197@st.jmi.ac.in>
2025-03-26 14:34:26 +0530

    Enhance Stats Component with Framer Motion Animations
    - Integrated Framer Motion for improved animations in the Stats component, enhancing user engagement as elements come into view.
    - Wrapped content in motion divs to create smoother transitions and visual appeal.
    - Cleaned up the structure for better readability and maintainability, ensuring a consistent layout across different screen sizes.

--- src/app/LandingPage/Stats.jsx
+++ src/app/LandingPage/Stats.jsx
@@ -1,10 +1,14 @@
-import React from "react";
 import bg from "../../assets/IMMLEGACYBG.jpg";
 import Heading from "../../components/Heading";
-import WordPullUp from "../../components/ui/word-pull-up";
 import NumberTicker from "../../components/ui/number-ticker";
+import { motion } from "framer-motion";
+import { useInView } from "framer-motion";
+import { useRef } from "react";
 
 const Stats = () => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true, margin: "-100px" });
+
   const stats = [
     { data: "20000", title: "Alumni Base" },
     { data: "56", title: "Years of Academic Excellence" },
@@ -15,7 +19,7 @@ const Stats = () => {
   ];
 
   return (
-    <section className=" relative px-0 py-10 lg:py-20 md:py-12 sm:px-0">
+    <section className="relative px-0 py-10 lg:py-20 md:py-12 sm:px-0">
       <img
         src={bg}
         alt="stats"
@@ -23,82 +27,115 @@ const Stats = () => {
       />
       <div className="absolute inset-0 z-20 bg-black opacity-20" />
       <div className="relative z-30 max-w-screen-xl px-4 mx-auto md:px-8">
-      
-        <Heading
-          title="IMM LEGACY" 
-          subtitle="Over five decades of shaping global leaders with 20,000+ successful alumni, hundreds of accolades, and partnerships worldwide."
-          titleClassName="text-4xl md:text-6xl font-bold text-yellow-400"
-          subtitleClassName="text-gray-300 text-base md:text-lg"
-        />
+        <motion.div
+          ref={ref}
+          initial={{ opacity: 0, y: 100 }}
+          animate={{ opacity: isInView ? 1 : 0, y: isInView ? 0 : 100 }}
+          transition={{ duration: 1.5, ease: "easeOut", delay: 0.5 }}
+        >
+          <motion.div
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: isInView ? 1 : 0, y: isInView ? 0 : 50 }}
+            transition={{ duration: 1.5, delay: 1 }}
+          >
+            <Heading
+              title="IMM LEGACY"
+              subtitle="Over five decades of shaping global leaders with 20,000+ successful alumni, hundreds of accolades, and partnerships worldwide."
+              titleClassName="text-4xl md:text-6xl font-bold text-yellow-400"
+              subtitleClassName="text-gray-300 text-base md:text-lg"
+            />
+          </motion.div>
 
-        {/* Example of Heading without subtitle */}
+          <motion.div
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: isInView ? 1 : 0, y: isInView ? 0 : 50 }}
+            transition={{ duration: 1.5, delay: 1.5 }}
+            className="hidden sm:grid grid-cols-2 gap-8 md:grid-cols-3 divide-x-2 divide-yellow-500 md:gap-0 md:divide-yellow-500"
+          >
+            {stats.slice(0, 3).map((item, index) => (
+              <motion.div
+                key={index}
+                initial={{ opacity: 0, y: 30 }}
+                animate={{ opacity: isInView ? 1 : 0, y: isInView ? 0 : 30 }}
+                transition={{ duration: 1, delay: 2 + index * 0.2 }}
+                className="flex flex-col items-center md:p-4"
+              >
+                <div className="text-5xl font-bold text-white font sm:text-6xl">
+                  <NumberTicker
+                    value={item.data}
+                    className="text-4xl font-bold text-yellow-500 font sm:text-6xl"
+                  />{" "}
+                  <span className="-ml-3 text-4xl font-bold text-yellow-500 font sm:text-6xl">
+                    +
+                  </span>
+                </div>
+                <div className="mt-3 text-sm font-medium text-gray-400 sm:text-xl text-center max-w-32 sm:max-w-none">
+                  {item.title}
+                </div>
+              </motion.div>
+            ))}
+          </motion.div>
 
-        <div className="hidden sm:grid grid-cols-2 gap-8 md:grid-cols-3 divide-x-2 divide-yellow-500  md:gap-0  md:divide-yellow-500">
-          {stats.slice(0, 3).map((item, index) => (
-            <div key={index} className="flex flex-col items-center md:p-4">
-              <div className="text-5xl font-bold text-white font sm:text-6xl">
-                {/* {item.data} */}
-                <NumberTicker
-                  value={item.data}
-                  className="text-4xl font-bold text-yellow-500 font sm:text-6xl"
-                />{" "}
-                
+          <motion.div
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: isInView ? 1 : 0, y: isInView ? 0 : 50 }}
+            transition={{ duration: 1.5, delay: 2.5 }}
+            className="hidden sm:grid grid-cols-2 gap-8 md:grid-cols-3 mt-8 divide-x-2 divide-yellow-500 md:gap-0 md:divide-yellow-500"
+          >
+            {stats.slice(3, 6).map((item, index) => (
+              <motion.div
+                key={index}
+                initial={{ opacity: 0, y: 30 }}
+                animate={{ opacity: isInView ? 1 : 0, y: isInView ? 0 : 30 }}
+                transition={{ duration: 1, delay: 3 + index * 0.2 }}
+                className="flex flex-col items-center md:p-4"
+              >
+                <div className="text-5xl font-bold text-white font sm:text-6xl">
+                  <NumberTicker
+                    value={item.data}
+                    className="text-4xl font-bold text-yellow-500 font sm:text-6xl"
+                  />{" "}
                   <span className="-ml-3 text-4xl font-bold text-yellow-500 font sm:text-6xl">
                     +
                   </span>
-             
-              </div>
-              <div className="mt-3 text-sm font-medium text-gray-400 sm:text-xl text-center max-w-32 sm:max-w-none">
-                {item.title}
-                {}
-              </div>
-            </div>
-          ))}
-        </div>
-        <div className="hidden sm:grid grid-cols-2 gap-8 md:grid-cols-3 mt-8 divide-x-2 divide-yellow-500  md:gap-0  md:divide-yellow-500">
-          {stats.slice(3, 6).map((item, index) => (
-            <div key={index} className="flex flex-col items-center md:p-4">
-              <div className="text-5xl font-bold text-white font sm:text-6xl">
-                {/* {item.data} */}
-                <NumberTicker
-                  value={item.data}
-                  className="text-4xl font-bold text-yellow-500 font sm:text-6xl"
-                />{" "}
-                
+                </div>
+                <div className="mt-3 text-sm font-medium text-gray-400 sm:text-xl text-center max-w-32 sm:max-w-none">
+                  {item.title}
+                </div>
+              </motion.div>
+            ))}
+          </motion.div>
+
+          <motion.div
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: isInView ? 1 : 0, y: isInView ? 0 : 50 }}
+            transition={{ duration: 1.5, delay: 3.5 }}
+            className="sm:hidden grid grid-cols-2 gap-8 md:grid-cols-3 mt-8 sm:divide-x-2 divide-yellow-500 md:gap-0 md:divide-yellow-500"
+          >
+            {stats.map((item, index) => (
+              <motion.div
+                key={index}
+                initial={{ opacity: 0, x: index % 2 === 0 ? -50 : 50 }}
+                animate={{ opacity: isInView ? 1 : 0, x: isInView ? 0 : (index % 2 === 0 ? -50 : 50) }}
+                transition={{ duration: 1, delay: 3.5 + index * 0.2 }}
+                className="flex flex-col items-center md:p-4"
+              >
+                <div className="text-5xl font-bold text-white font sm:text-6xl">
+                  <NumberTicker
+                    value={item.data}
+                    className="text-4xl font-bold text-yellow-500 font sm:text-6xl"
+                  />{" "}
                   <span className="-ml-3 text-4xl font-bold text-yellow-500 font sm:text-6xl">
                     +
                   </span>
-             
-              </div>
-              <div className="mt-3 text-sm font-medium text-gray-400 sm:text-xl text-center max-w-32 sm:max-w-none">
-                {item.title}
-                {}
-              </div>
-            </div>
-          ))}
-        </div>
-        <div className="sm:hidden grid grid-cols-2 gap-8 md:grid-cols-3 mt-8 sm:divide-x-2 divide-yellow-500  md:gap-0  md:divide-yellow-500">
-          {stats.map((item, index) => (
-            <div key={index} className="flex flex-col items-center md:p-4">
-              <div className="text-5xl font-bold text-white font sm:text-6xl">
-                {/* {item.data} */}
-                <NumberTicker
-                  value={item.data}
-                  className="text-4xl font-bold text-yellow-500 font sm:text-6xl"
-                />{" "}
-                
-                  <span className="-ml-3 text-4xl font-bold text-yellow-500 font sm:text-6xl">
-                    +
-                  </span>
-             
-              </div>
-              <div className="mt-3 text-sm font-medium text-gray-400 sm:text-xl text-center max-w-32 sm:max-w-none">
-                {item.title}
-                {}
-              </div>
-            </div>
-          ))}
-        </div>
+                </div>
+                <div className="mt-3 text-sm font-medium text-gray-400 sm:text-xl text-center max-w-32 sm:max-w-none">
+                  {item.title}
+                </div>
+              </motion.div>
+            ))}
+          </motion.div>
+        </motion.div>
       </div>
     </section>
   );
