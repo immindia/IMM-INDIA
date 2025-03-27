6f0b920eac43f421e8c65f9e620ad124a4171d7a
whyehtasam <ehtasam2003197@st.jmi.ac.in>
2025-03-27 13:02:52 +0530

    Implement Scroll-Responsive Header with Visibility Toggle
    - Added state management to control header visibility based on scroll position, enhancing user experience.
    - Introduced useEffect hook to handle scroll events and update header visibility with a delay.
    - Refactored header structure to improve layout and maintainability, ensuring a smooth transition effect.

--- src/app/header/Header.jsx
+++ src/app/header/Header.jsx
@@ -1,12 +1,40 @@
-import TopBar from './TopBar'
-import Navbar from './Navbar'
-const Header = () => {
-  return (
-    <>
-    <TopBar />
-    <Navbar />
-    </>
-  )
-}
+import TopBar from "./TopBar";
+import Navbar from "./Navbar";
+import { useEffect, useState } from "react";
+// If you have these resources, uncomment these imports:
+// import logo from "../../assets/logo.png"
+// import Drawer from "./Drawer"
 
-export default Header
+const Header = () => {
+  const [isVisible, setIsVisible] = useState(true);
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.scrollY;
+
+      // Add delay using setTimeout
+      setTimeout(() => {
+        setIsVisible(prevScrollPos > currentScrollPos || currentScrollPos < 10);
+        setPrevScrollPos(currentScrollPos);
+      }, 150); // 100ms delay
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [prevScrollPos]);
+
+  return (
+    <header
+      className={`shadow-sm z-50  transition-all duration-500 ${
+        isVisible ? "sticky top-0 translate-y-0" : "relative -translate-y-full"
+      }`}
+    >
+      <TopBar />
+
+      <Navbar />
+    </header>
+  );
+};
+
+export default Header;
