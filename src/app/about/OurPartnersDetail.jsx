b20cf62b68150c8a6ebd3513ad7a4587eb2db790
whyehtasam <ehtasam2003197@st.jmi.ac.in>
2025-04-11 11:12:26 +0530

    Update Kladio image format in OurPartnersDetail component from PNG to WEBP for improved loading performance

--- src/app/about/OurPartnersDetail.jsx
+++ src/app/about/OurPartnersDetail.jsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 import EU from "../../assets/partners/EU.png";
 import IIT from "../../assets/partners/iit.png";
 import JIT from "../../assets/partners/jit.png";
-import Kladio from "../../assets/partners/kladio.png";
+import Kladio from "../../assets/partners/kladio.webp";
 import Vitti from "../../assets/partners/vitti.png";
 import Wasme from "../../assets/partners/wasme.png";
 import Young from "../../assets/partners/young.png";
