5c0d4560dd6db30f777de0df2532bec971dee2cd
Yogesh Nandal <yogidesign1996@gmail.com>
2025-06-11 10:14:27 +0530

    Update marquee text in MarqueeContext for clarity on PGDM session dates

--- src/context/MarqueeContext.jsx
+++ src/context/MarqueeContext.jsx
@@ -6,7 +6,7 @@ export const useMarquee = () => useContext(MarqueeContext);
 
 export const MarqueeProvider = ({ children }) => {
   const [marqueeText, setMarqueeText] = useState(
-    "Phase 3 Applications Closed for PGDM 2025-2027"
+    "PGDM Session 2025-27 Commences from July 7, 2025, onwards for Batch 1*"
   );
   const [isBBAPage, setIsBBAPage] = useState(false);
 
