4565b6d2671f0122869cdb37f466209587eb813e
Yogesh Nandal <yogidesign1996@gmail.com>
2025-06-12 17:44:51 +0530

    Refactor MobilePopup component to remove unnecessary height class for improved layout

--- src/app/LandingPage/Landing.jsx
+++ src/app/LandingPage/Landing.jsx
@@ -36,7 +36,7 @@ const MobilePopup = () => {
 
   return (
     <Dialog open={isOpen} onOpenChange={setIsOpen}>
-      <DialogContent className="md:hidden z-[9999] p-0 w-full h-full px-5 py-10  bg-transparent border-0 rounded-lg overflow-hidden focus:outline-none">
+      <DialogContent className="md:hidden z-[9999] w-full h-full px-5 py-10  bg-transparent border-0 rounded-lg overflow-hidden focus:outline-none">
         <a
           href="https://www.immindia.edu.in/imm/general-pgdm-lp/"
           target="_blank"
