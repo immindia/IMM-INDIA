abd023f286a77bb7f78a2774e474c21c19264787
whyehtasam <ehtasam2003197@st.jmi.ac.in>
2025-07-11 15:39:29 +0530

    Update marquee text in MarqueeContext for accuracy; replace pop-up image with new version.

--- src/context/MarqueeContext.jsx
+++ src/context/MarqueeContext.jsx
@@ -7,7 +7,7 @@ export const useMarquee = () => useContext(MarqueeContext);
 
 export const MarqueeProvider = ({ children }) => {
   const [marqueeText, setMarqueeText] = useState(
-    "PGDM Session 2025-27 Commences from August 11, 2025, onwards for Batch 2* | Phase-4 Applications Closed"
+    "PGDM Session 2025-27 Commences from August 18, 2025, onwards for Batch 2* | Phase-4 Applications Closed"
   );
   const [isBBAPage, setIsBBAPage] = useState(false);
 
